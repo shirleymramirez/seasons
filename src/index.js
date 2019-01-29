e915de196878b1eddbbc17f93be87843e6019b12
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-01-29 16:50:17 -0700

    added conditional statement for rendering JSX

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   render() {
-    return <div> Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
