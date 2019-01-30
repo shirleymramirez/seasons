db20e0342b2f1cee031090a02fc1359a579c9051
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-01-30 11:11:03 -0700

    refactored SeasonDisplay.js, useing props. Used ternary operator for logic test

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display!</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, its chilly!" : "lets hit the beach!";
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
