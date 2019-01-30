d3fdff29ab75a03ffa35f7ce248ab968e3861edf
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-01-30 10:35:11 -0700

    added refactored code to add componentDidMount

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
